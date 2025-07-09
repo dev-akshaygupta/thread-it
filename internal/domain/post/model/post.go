ca48a913a74223a4e6f1aeee45a8b302132ac34a
Akshay Gupta <dev.akshaygupta@gmail.com>
2025-07-09 14:53:18 -0400

    defined model and service

--- internal/domain/post/model/post.go
+++ internal/domain/post/model/post.go
@@ -1,1 +1,13 @@
 package model
+
+import "time"
+
+type Post struct {
+	ID          string
+	Title       string
+	Body        string
+	UserID      string
+	SubredditID string
+	CreatedAt   time.Time
+	VoteCount   int
+}
--- internal/domain/post/service/service.go
+++ internal/domain/post/service/service.go
@@ -1,1 +1,11 @@
 package service
+
+import (
+	"github.com/dev-akshaygupta/thread-it/internal/domain/post/model"
+)
+
+type PostService interface {
+	CreatePost(userID string, req model.CreatePostRequest) (*model.Post, error)
+	GetPostByID(postID string) (*model.Post, error)
+	ListPosts(limit, offset int) ([]*model.Post, error)
+}
